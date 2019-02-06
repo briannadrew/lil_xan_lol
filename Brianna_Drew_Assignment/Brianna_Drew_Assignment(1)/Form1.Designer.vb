b68ccd55a0a63fab5c66a907805df2206065d638
briannadrew <briannadrew@trentu.ca>
2019-02-05 22:45:53 -0500

    commit

---
+++ Brianna_Drew_Assignment/Brianna_Drew_Assignment(1)/Form1.Designer.vb
@@ -0,0 +1,274 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Brianna_Drew_Assignment_1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Brianna_Drew_Assignment_1))
+        Me.RedButton = New System.Windows.Forms.Button()
+        Me.BlueButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.TextBox = New System.Windows.Forms.TextBox()
+        Me.EnterButton = New System.Windows.Forms.Button()
+        Me.ChangingLabel = New System.Windows.Forms.Label()
+        Me.URLbutton = New System.Windows.Forms.Button()
+        Me.puggif2 = New System.Windows.Forms.PictureBox()
+        Me.puggif1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        CType(Me.puggif2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.puggif1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'RedButton
+        '
+        Me.RedButton.BackColor = System.Drawing.Color.Red
+        Me.RedButton.Font = New System.Drawing.Font("Old English Text MT", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RedButton.Location = New System.Drawing.Point(402, 191)
+        Me.RedButton.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.RedButton.Name = "RedButton"
+        Me.RedButton.Size = New System.Drawing.Size(135, 48)
+        Me.RedButton.TabIndex = 0
+        Me.RedButton.Text = "Red"
+        Me.RedButton.UseVisualStyleBackColor = False
+        '
+        'BlueButton
+        '
+        Me.BlueButton.BackColor = System.Drawing.Color.Blue
+        Me.BlueButton.Font = New System.Drawing.Font("Old English Text MT", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BlueButton.Location = New System.Drawing.Point(583, 191)
+        Me.BlueButton.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.BlueButton.Name = "BlueButton"
+        Me.BlueButton.Size = New System.Drawing.Size(135, 48)
+        Me.BlueButton.TabIndex = 1
+        Me.BlueButton.Text = "Blue"
+        Me.BlueButton.UseVisualStyleBackColor = False
+        '
+        'ExitButton
+        '
+        Me.ExitButton.BackColor = System.Drawing.Color.DeepPink
+        Me.ExitButton.Font = New System.Drawing.Font("Old English Text MT", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ExitButton.Location = New System.Drawing.Point(959, 497)
+        Me.ExitButton.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(135, 48)
+        Me.ExitButton.TabIndex = 2
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = False
+        '
+        'TextBox
+        '
+        Me.TextBox.BackColor = System.Drawing.Color.BlueViolet
+        Me.TextBox.Font = New System.Drawing.Font("Old English Text MT", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox.Location = New System.Drawing.Point(279, 309)
+        Me.TextBox.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.TextBox.Multiline = True
+        Me.TextBox.Name = "TextBox"
+        Me.TextBox.Size = New System.Drawing.Size(564, 126)
+        Me.TextBox.TabIndex = 3
+        Me.TextBox.Text = "Type Here and Click Submit!"
+        '
+        'EnterButton
+        '
+        Me.EnterButton.BackColor = System.Drawing.Color.DarkOrange
+        Me.EnterButton.Font = New System.Drawing.Font("Old English Text MT", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.EnterButton.Location = New System.Drawing.Point(848, 385)
+        Me.EnterButton.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.EnterButton.Name = "EnterButton"
+        Me.EnterButton.Size = New System.Drawing.Size(176, 48)
+        Me.EnterButton.TabIndex = 4
+        Me.EnterButton.Text = "Submit"
+        Me.EnterButton.UseVisualStyleBackColor = False
+        '
+        'ChangingLabel
+        '
+        Me.ChangingLabel.AllowDrop = True
+        Me.ChangingLabel.AutoSize = True
+        Me.ChangingLabel.BackColor = System.Drawing.Color.GreenYellow
+        Me.ChangingLabel.Font = New System.Drawing.Font("Old English Text MT", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ChangingLabel.Location = New System.Drawing.Point(486, 74)
+        Me.ChangingLabel.Name = "ChangingLabel"
+        Me.ChangingLabel.Size = New System.Drawing.Size(148, 40)
+        Me.ChangingLabel.TabIndex = 5
+        Me.ChangingLabel.Text = "Welcome!"
+        Me.ChangingLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'URLbutton
+        '
+        Me.URLbutton.BackColor = System.Drawing.Color.Yellow
+        Me.URLbutton.Font = New System.Drawing.Font("Old English Text MT", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.URLbutton.Location = New System.Drawing.Point(171, 499)
+        Me.URLbutton.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.URLbutton.Name = "URLbutton"
+        Me.URLbutton.Size = New System.Drawing.Size(217, 48)
+        Me.URLbutton.TabIndex = 6
+        Me.URLbutton.Text = "❤Fun❤"
+        Me.URLbutton.UseVisualStyleBackColor = False
+        '
+        'puggif2
+        '
+        Me.puggif2.BackColor = System.Drawing.Color.Transparent
+        Me.puggif2.Image = Global.Brianna_Drew_Assignment_1_.My.Resources.Resources.funny_cute_animated_pug_gif_32
+        Me.puggif2.Location = New System.Drawing.Point(760, 83)
+        Me.puggif2.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.puggif2.Name = "puggif2"
+        Me.puggif2.Size = New System.Drawing.Size(321, 201)
+        Me.puggif2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.puggif2.TabIndex = 7
+        Me.puggif2.TabStop = False
+        '
+        'puggif1
+        '
+        Me.puggif1.BackColor = System.Drawing.Color.Transparent
+        Me.puggif1.Image = Global.Brianna_Drew_Assignment_1_.My.Resources.Resources.ezgif_3_cda0d313bc
+        Me.puggif1.Location = New System.Drawing.Point(37, 83)
+        Me.puggif1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.puggif1.Name = "puggif1"
+        Me.puggif1.Size = New System.Drawing.Size(321, 209)
+        Me.puggif1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.puggif1.TabIndex = 8
+        Me.puggif1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Black
+        Me.Label1.Font = New System.Drawing.Font("OCR A Extended", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label1.Location = New System.Drawing.Point(-1, 478)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(528, 18)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Click the button below to see a funny YouTube video!"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Black
+        Me.Label2.Font = New System.Drawing.Font("OCR A Extended", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label2.Location = New System.Drawing.Point(311, 251)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(455, 15)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Click the buttons above to change the background colour!"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Black
+        Me.Label3.Font = New System.Drawing.Font("OCR A Extended", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label3.Location = New System.Drawing.Point(820, 328)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(207, 15)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "When you click the submit"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Black
+        Me.Label4.Font = New System.Drawing.Font("OCR A Extended", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label4.Location = New System.Drawing.Point(811, 342)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(231, 15)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "button, the text you entered"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.Color.Black
+        Me.Label5.Font = New System.Drawing.Font("OCR A Extended", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label5.Location = New System.Drawing.Point(811, 355)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(223, 15)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "appears in the green box at"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.BackColor = System.Drawing.Color.Black
+        Me.Label6.Font = New System.Drawing.Font("OCR A Extended", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label6.Location = New System.Drawing.Point(834, 369)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(183, 15)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "the top of the window."
+        '
+        'Brianna_Drew_Assignment_1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.BackgroundImage = Global.Brianna_Drew_Assignment_1_.My.Resources.Resources.xan
+        Me.ClientSize = New System.Drawing.Size(1114, 591)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.puggif1)
+        Me.Controls.Add(Me.puggif2)
+        Me.Controls.Add(Me.URLbutton)
+        Me.Controls.Add(Me.ChangingLabel)
+        Me.Controls.Add(Me.EnterButton)
+        Me.Controls.Add(Me.TextBox)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.BlueButton)
+        Me.Controls.Add(Me.RedButton)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Name = "Brianna_Drew_Assignment_1"
+        Me.Text = "Brianna Drew: Assignment 1"
+        CType(Me.puggif2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.puggif1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents RedButton As Button
+    Friend WithEvents BlueButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents TextBox As TextBox
+    Friend WithEvents EnterButton As Button
+    Friend WithEvents ChangingLabel As Label
+    Friend WithEvents URLbutton As Button
+    Friend WithEvents puggif2 As PictureBox
+    Friend WithEvents puggif1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+End Class
---
+++ Brianna_Drew_Assignment/Brianna_Drew_Assignment(1)/Form1.vb
@@ -0,0 +1,98 @@
+﻿
+'
+' This is Brianna Drew's introduction to Visual Basic Routine.
+'
+' Written by: Brianna Drew (0622446) - January 2018
+'
+' Purpose: To demonstrate knowledge on the basics of Visual Basic (Using buttons, labels, and text
+' boxes to control things such as background colour/image, transferring of text via a variable,
+' ending the program, and many other features).
+'
+' Parameters: None
+'
+' Subroutine Required: Each button on the form has it's own subroutine as well as the text box and the form itself.
+'
+' Modifications: - January 23/18 - Program started, basic features dragged to form (buttons, text box, label).
+'                - January 24/18 - Functions added to some of the buttons (changing background color/image).
+'                - January 25/18 - Functions added to textbox, submit button, exit button and green label.
+'                - January 29/18 - Rest of labels added, pug gifs added, fun button added along with its function.
+'                - January 30/18 - Inline code documentation (comments) added.
+'                - February 1/18 - Intro block comment last updated/finished.
+'
+
+Public Class Brianna_Drew_Assignment_1
+
+
+    Private Sub Brianna_Drew_Assignment_1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' This subroutine runs as soon as the application begins. It sends the pug gifs to the back of the 
+        ' form so they do not block other features.
+
+        puggif1.SendToBack() ' Send the first pug gif to the back on the form.
+        puggif2.SendToBack() ' Send the second pug gif to the back on the form.
+
+    End Sub
+
+
+    Private Sub Form1_Click(sender As Object, e As EventArgs) Handles MyBase.Click
+        ' This subroutine runs when the base/background of the form is clicked.
+
+        Me.BackColor = Nothing ' Removes background colour so we can actually see the image of lil xan.
+        BackgroundImage = My.Resources.xan ' Sets background image to tiled image of the rapper, lil xan.
+
+    End Sub
+
+
+    Private Sub BlueButton_Click(sender As Object, e As EventArgs) Handles BlueButton.Click
+        ' This subroutine runs when the blue button that says "Blue" is clicked.
+
+        BackColor = Color.Blue ' Sets the background colour to blue.
+        Me.BackgroundImage = Nothing ' Removes background image so we can actually see the background colour.
+
+    End Sub
+
+
+    Private Sub RedButton_Click(sender As Object, e As EventArgs) Handles RedButton.Click
+        ' This subroutine runs when the red button that says "Red" is clicked.
+
+        BackColor = Color.Red ' Sets the background colour to red.
+        Me.BackgroundImage = Nothing ' Removes background image so we can actually see the background colour.
+
+    End Sub
+
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        ' This subroutine runs when the pink button that says "Exit" is clicked.
+
+        End ' Ends and closes the application.
+
+    End Sub
+
+
+    Private Sub TextBox_Click(sender As Object, e As EventArgs) Handles TextBox.Click
+        ' This subroutine runs when the purple textbox is clicked.
+
+        TextBox.Clear() ' Clears the text from the textbox.
+
+    End Sub
+
+
+    Private Sub EnterButton_Click(sender As Object, e As EventArgs) Handles EnterButton.Click
+        ' This subroutine runs when the orange button that says "Submit" is clicked.
+
+        Dim userText As String ' Defines a variable that will store what the user types into the textbox.
+        userText = TextBox.Text ' Stores what the user types into the textbox into that variable.
+        ChangingLabel.Text = userText ' Changes the green label near the top of form to read what the user typed.
+
+    End Sub
+
+
+    Private Sub URLbutton_Click(sender As Object, e As EventArgs) Handles URLbutton.Click
+        ' This subroutine runs when the yellow button that says "❤Fun❤" is clicked.
+
+        Process.Start("https://www.youtube.com/watch?v=N3WeXGmqYsE") ' Will open a funny YouTube video in a new tab
+        'in your default internet browser.
+
+    End Sub
+
+
+End Class
